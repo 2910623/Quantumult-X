f33dfe9d92b19d577d296a87c57f42713f46f941
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:59:26 +0800

    Update keep.js

--- pyy/ycdz/keep.js
+++ pyy/ycdz/keep.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.gotokeep\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/keep.js
+^https:\/\/api\.gotokeep\.com url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/keep.js
 
 [mitm] 
 
