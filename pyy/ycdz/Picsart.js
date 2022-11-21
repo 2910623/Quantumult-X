45d24c7dd71394e2b32a9a8b13752cef9799363c
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:22:06 +0800

    Update Picsart.js

--- pyy/ycdz/Picsart.js
+++ pyy/ycdz/Picsart.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/api\.meiease\.cn\/shop\/subscription\/validate url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Picsart.js
+^https:\/\/api\.meiease\.cn\/shop\/subscription\/validate url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/Picsart.js
 
 [mitm] 
 
