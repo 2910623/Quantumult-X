e59e3ca3bf107bd2b57326f7f1f23be4a037923f
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:31:23 +0800

    Update abushuiqiangushi.js

--- pyy/ycdz/abushuiqiangushi.js
+++ pyy/ycdz/abushuiqiangushi.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/abushuiqiangushi.js
+http:\/\/tutugushiapi\.tutuerge\.com url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/abushuiqiangushi.js
 
 [mitm] 
 
