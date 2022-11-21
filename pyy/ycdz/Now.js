983f908bade56e42d2f37faba304eeea0cf92cf5
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 15:40:13 +0800

    Update Now.js

--- pyy/ycdz/Now.js
+++ pyy/ycdz/Now.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/nowapi\.navoinfo\.cn url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Now.js
+^https:\/\/nowapi\.navoinfo\.cn url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/Now.js
 
 [mitm] 
 
