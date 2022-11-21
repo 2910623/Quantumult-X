0d045c22d24ec7378d5f88fa04fc87a3c04b390d
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:41:37 +0800

    Update egdd.js

--- pyy/ycdz/egdd.js
+++ pyy/ycdz/egdd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/gateway\.ergedd\.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/egdd.js
+http:\/\/gateway\.ergedd\.com url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/egdd.js
 
 [mitm] 
 
