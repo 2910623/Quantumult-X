ebc88335f83bb407c2e49c9cd7bc6ec7c610637c
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:31:46 +0800

    Update aihlx.js

--- pyy/ycdz/aihlx.js
+++ pyy/ycdz/aihlx.js
@@ -14,7 +14,7 @@ TG反馈群：https://t.me/plus8889
 *******************************
 [rewrite_local]
 
-^https:\/\/changeclothes\.szsszykj\.com\/interface\/GetIosVip.php url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/aihlx.js
+^https:\/\/changeclothes\.szsszykj\.com\/interface\/GetIosVip.php url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/aihlx.js
 
 [mitm]
 hostname = changeclothes.szsszykj.com
