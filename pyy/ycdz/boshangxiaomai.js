7488072ba7ae58712c17009e549e366dc9bff111
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:35:24 +0800

    Update boshangxiaomai.js

--- pyy/ycdz/boshangxiaomai.js
+++ pyy/ycdz/boshangxiaomai.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/yyy\.bosum\.com\/enterprise url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/boshangxiaomai.js
+http:\/\/yyy\.bosum\.com\/enterprise url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/boshangxiaomai.js
 
 [mitm] 
 
