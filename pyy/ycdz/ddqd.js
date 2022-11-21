be7b245ed9d8d13a930b4ee861a83a0554d012dd
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:38:49 +0800

    Update ddqd.js

--- pyy/ycdz/ddqd.js
+++ pyy/ycdz/ddqd.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ddqd.js
+^https:\/\/dida365\.com\/api\/v2\/user\/status url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/ddqd.js
 
 [mitm] 
 
