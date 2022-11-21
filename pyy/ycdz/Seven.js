f62a7aec65212de901c5e63814516a344e4e94e7
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:26:08 +0800

    Update Seven.js

--- pyy/ycdz/Seven.js
+++ pyy/ycdz/Seven.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://api.sevenworkouts.com/api/v2/verify-purchases url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Seven.js
+https://api.sevenworkouts.com/api/v2/verify-purchases url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/Seven.js
 
 [mitm] 
 
