ecf9c861886980f1ecc1472682f9f28fda895ae6
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 14:41:40 +0800

    Update FitnessView.js

--- pyy/ycdz/FitnessView.js
+++ pyy/ycdz/FitnessView.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/FitnessView.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/FitnessView.js
 
 [mitm] 
 
