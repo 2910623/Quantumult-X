4c6f95fa97745b92bd242b2b17a2ddbe12d19557
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:49:48 +0800

    Update gql.js

--- pyy/ycdz/gql.js
+++ pyy/ycdz/gql.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/expire\.vanemu\.com\/1\.1\/functions\/getUserInfo url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/gql.js
+^https:\/\/expire\.vanemu\.com\/1\.1\/functions\/getUserInfo url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/gql.js
 
 [mitm] 
 
