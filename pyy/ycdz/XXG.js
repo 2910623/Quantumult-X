44584a377478ffc215c803b18a99790e2706e82c
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:30:53 +0800

    Update XXG.js

--- pyy/ycdz/XXG.js
+++ pyy/ycdz/XXG.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/xianbeikeji\.com\/daily\/app\/user\/query url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/XXG.js
+^https:\/\/xianbeikeji\.com\/daily\/app\/user\/query url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/XXG.js
 
 [mitm] 
 
