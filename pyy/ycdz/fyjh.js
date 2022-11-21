eccbb29fcf30c7723d62e0635eee7cf8e6f0ae34
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:44:22 +0800

    Update fyjh.js

--- pyy/ycdz/fyjh.js
+++ pyy/ycdz/fyjh.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/getway-plan\.abuddz\.com\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/fyjh.js
+^https:\/\/getway-plan\.abuddz\.com\/api\/sync\/putNeedUpdateData url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/fyjh.js
 
 [mitm] 
 
