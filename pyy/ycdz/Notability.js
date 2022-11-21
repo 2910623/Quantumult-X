b99d3fdae9809f3569073f6d744f0dad234dc058
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 15:39:41 +0800

    Update Notability.js

--- pyy/ycdz/Notability.js
+++ pyy/ycdz/Notability.js
@@ -18,7 +18,7 @@ TG频道群：https://t.me/py996
 [rewrite_local]
 
 
-^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Notability.js
+^https?:\/\/notability\.com\/subscriptions url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/Notability.js
 
 [mitm] 
 
