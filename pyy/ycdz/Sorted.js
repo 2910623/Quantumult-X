7ae96e4ff4462e9bf9fd52f0514adba0c062e044
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:26:28 +0800

    Update Sorted.js

--- pyy/ycdz/Sorted.js
+++ pyy/ycdz/Sorted.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py
 
 [rewrite_local]
 
-^https:\/\/api\.sortedapp\.com\/receipts\/verify url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/Sorted.js
+^https:\/\/api\.sortedapp\.com\/receipts\/verify url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/Sorted.js
 
 [mitm] 
 
