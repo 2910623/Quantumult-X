237aeadd479f5b0aa3110127ef0f80933b5f608f
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:56:37 +0800

    Update hzk.js

--- pyy/ycdz/hzk.js
+++ pyy/ycdz/hzk.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^http[s]?:\/\/www.zhongkao66.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/hzk.js
+^http[s]?:\/\/www.zhongkao66.com url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/hzk.js
 
 [mitm] 
 
