a7ce460611fb6e34afd6b531855b6e08f69b303a
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 17:11:22 +0800

    Update ljtq.js

--- pyy/ycdz/ljtq.js
+++ pyy/ycdz/ljtq.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/ljwapi\.baichuan\.tech url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ljtq.js
+^https:\/\/ljwapi\.baichuan\.tech url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/ljtq.js
 
 [mitm] 
 
