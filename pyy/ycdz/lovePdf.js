baded6fd3d6de3efb257683744cb1249715fad8b
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 17:12:19 +0800

    Update lovePdf.js

--- pyy/ycdz/lovePdf.js
+++ pyy/ycdz/lovePdf.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-https://service.ilovepdf.com/v1/user url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/lovePdf.js
+https://service.ilovepdf.com/v1/user url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/lovePdf.js
 
 
 [mitm] 
