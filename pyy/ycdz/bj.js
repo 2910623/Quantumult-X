f840dbfe5e2105327072774f79a44fc14e382311
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-21 16:34:17 +0800

    Update bj.js

--- pyy/ycdz/bj.js
+++ pyy/ycdz/bj.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https:\/\/user-kaji-api\.b612kaji\.com\/v1\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/bj.js
+^https:\/\/user-kaji-api\.b612kaji\.com\/v1\/purchase\/subscription\/subscriber\/status url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/bj.js
 
 [mitm] 
 hostname = user-kaji-api.b612kaji.com
