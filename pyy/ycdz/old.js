bc007d1f0ccd19085e8c90c5deca928d779ec5a5
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:25:20 +0800

    Update old.js

--- pyy/ycdz/old.js
+++ pyy/ycdz/old.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/old.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/old.js
 
 [mitm] 
 hostname = buy.itunes.apple.com
