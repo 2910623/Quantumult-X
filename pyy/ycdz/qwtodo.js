6b5daf2f4b905181094a605c8d731fd72a868618
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:29:13 +0800

    Update qwtodo.js

--- pyy/ycdz/qwtodo.js
+++ pyy/ycdz/qwtodo.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/frogtodo\.shuge888\.com:33333\/account\/refreshState url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/qwtodo.js
+^https:\/\/frogtodo\.shuge888\.com:33333\/account\/refreshState url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/qwtodo.js
 
 [mitm] 
 
