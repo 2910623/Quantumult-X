d243f5cc89938668d8a3fc52a5e9a22b71d63193
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:36:46 +0800

    Update vivacut.js

--- pyy/ycdz/vivacut.js
+++ pyy/ycdz/vivacut.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/medi\.dxzzy321\.top\/api\/rest\/commerce\/integrate\/vip\/perform url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/vivacut.js
+^https:\/\/medi\.dxzzy321\.top\/api\/rest\/commerce\/integrate\/vip\/perform url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/vivacut.js
 
 [mitm] 
 
