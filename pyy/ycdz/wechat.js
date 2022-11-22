3f2161fe5ab36428ed9a0d8cceb4ab1842d0a16a
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:37:23 +0800

    Update wechat.js

--- pyy/ycdz/wechat.js
+++ pyy/ycdz/wechat.js
@@ -15,7 +15,7 @@ TG反馈群：https://t.me/plus8889
 *******************************
 [rewrite_local]
 
-^https?://mp\.weixin\.qq\.com/s\?.* url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wechat.js
+^https?://mp\.weixin\.qq\.com/s\?.* url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/wechat.js
 
 [mitm]
 
