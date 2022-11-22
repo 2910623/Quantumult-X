9971664eb25b1116630c261473b06bd512b38fcb
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:39:53 +0800

    Update wnbsq.js

--- pyy/ycdz/wnbsq.js
+++ pyy/ycdz/wnbsq.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/www\.40sishi\.com\/voice\/user\/profile url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wnbsq.js
+^https:\/\/www\.40sishi\.com\/voice\/user\/profile url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/wnbsq.js
 
 [mitm] 
 
