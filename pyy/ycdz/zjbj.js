69bd29aec1017910ebf5968baf5196a0ce4cf1e4
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:49:12 +0800

    Update zjbj.js

--- pyy/ycdz/zjbj.js
+++ pyy/ycdz/zjbj.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-http:\/\/data\.ufbae\.com\/User\/GetUser url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/zjbj.js
+http:\/\/data\.ufbae\.com\/User\/GetUser url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/zjbj.js
 
 [mitm] 
 
