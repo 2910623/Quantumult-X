4976b70ce0f54f31ceafb30690f402c668cf6471
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:47:25 +0800

    Update ypjjtq.js

--- pyy/ycdz/ypjjtq.js
+++ pyy/ycdz/ypjjtq.js
@@ -16,7 +16,7 @@ TG频道群：https://t.me/py996
 
 [rewrite_local]
 
-^https:\/\/fufei\.fengyunsoft\.cn\/api\/client\/info url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/ypjjtq.js
+^https:\/\/fufei\.fengyunsoft\.cn\/api\/client\/info url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/ypjjtq.js
 
 [mitm] 
 
