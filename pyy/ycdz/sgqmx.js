aea70cd6c1d9aba9c98e4bca53b192e2125fedca
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:30:57 +0800

    Update sgqmx.js

--- pyy/ycdz/sgqmx.js
+++ pyy/ycdz/sgqmx.js
@@ -16,7 +16,7 @@ TG反馈群：https://t.me/plus8889
 
 [rewrite_local]
 
-^https:\/\/wx-xmlb\.lanfeitech\.com\/api\/api\/exchange_cdkey url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/sgqmx.js
+^https:\/\/wx-xmlb\.lanfeitech\.com\/api\/api\/exchange_cdkey url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/sgqmx.js
 
 [mitm] 
 
