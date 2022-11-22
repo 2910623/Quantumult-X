b5fd9b3e1161b0a962c69684d56b426c0e0646f6
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:35:36 +0800

    Update umu168.js

--- pyy/ycdz/umu168.js
+++ pyy/ycdz/umu168.js
@@ -11,7 +11,7 @@ TG频道群：https://t.me/py996
 *******************************
 [rewrite_local]
 
-^http[s]?:\/\/api3.umu168.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/umu168.js
+^http[s]?:\/\/api3.umu168.com url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/umu168.js
 
 [mitm] 
 
