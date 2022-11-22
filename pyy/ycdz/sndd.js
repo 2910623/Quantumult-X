e17de2af77e80b20b67fb1ba6d0de454c7cd55ec
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:32:58 +0800

    Update sndd.js

--- pyy/ycdz/sndd.js
+++ pyy/ycdz/sndd.js
@@ -17,7 +17,7 @@ TG频道群：https://t.me/py996
 [rewrite_local]
 
 ^https:\/\/igetcool-gateway\.igetcool\.com
- url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/sndd.js
+ url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/sndd.js
 
 [mitm] 
 
