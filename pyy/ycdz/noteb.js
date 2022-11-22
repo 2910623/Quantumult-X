3396212333799bf8a1bf24c49df402414eb8623b
Nikola Ds <118238230+2910623@users.noreply.github.com>
2022-11-22 08:24:37 +0800

    Update noteb.js

--- pyy/ycdz/noteb.js
+++ pyy/ycdz/noteb.js
@@ -13,7 +13,7 @@
 
 [rewrite_local]
 
-^https:\/\/subscription-api\.notedapp\.io\/api\/verifyReceipt url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/noteb.js
+^https:\/\/subscription-api\.notedapp\.io\/api\/verifyReceipt url script-response-body https://raw.githubusercontent.com/2910623/Quantumult-X/main/pyy/ycdz/noteb.js
 
 [mitm] 
 hostname = subscription-api.notedapp.io
